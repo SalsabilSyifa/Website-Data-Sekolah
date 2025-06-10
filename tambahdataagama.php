0854b5f3e638813f1789b91bafb9aa521181e467
MyBook Hype <as.zennitti@gmail.com>
2025-06-10 22:12:38 +0700

    mobile schreenshot

--- tambahdataagama.php
+++ tambahdataagama.php
@@ -149,7 +149,7 @@ if (isset($_POST['simpan'])) {
           <!--begin::Row-->
           <div class="row">
             <div class="col-sm-6">
-              <h3 class="mb-0">Data Jurusan</h3>
+              <h3 class="mb-0">Data Agama</h3>
             </div>
             <div class="col-sm-6">
               <ol class="breadcrumb float-sm-end">
@@ -173,7 +173,7 @@ if (isset($_POST['simpan'])) {
               <!-- /.card -->
               <div class="card mb-4">
                 <div class="card-header">
-                  <h3 class="card-title">Tambah Data Jurusan</h3>
+                  <h3 class="card-title">Tambah Data Agama</h3>
                 </div>
                 <!-- /.card-header -->
                 <div class="card-body p-0">
